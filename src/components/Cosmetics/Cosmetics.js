c2946aae560c7c4aad79e006b795222bf1192003
dipdastoko <dipdastoko@gmail.com>
2022-02-28 18:18:50 +0600

    export-import

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { add as addition, multiply } from '../../utilities/storage'
+
+const Cosmetics = () => {
+    const first = 55;
+    const second = 89;
+    const sum = addition(first, second);
+    const multi = multiply(first, second);
+    // const gap =
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Cosmetics;
