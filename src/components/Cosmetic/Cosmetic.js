fe1a32575e93f948d8067e78265549eca39fdbd9
dipdastoko <dipdastoko@gmail.com>
2022-03-01 01:57:27 +0600

    fakedb

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, removeFromDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
@@ -8,12 +8,17 @@ const Cosmetic = (props) => {
         console.log(id);
         addToDb(id);
     }
+
+    const handleRemove = id => {
+        removeFromDb(id);
+    }
     return (
         <div>
             <h2>name: {name}</h2>
             <h4>id:{id}</h4>
             <p>Price: {price} Taka</p>
             <button onClick={() => handlePurchase(id)}>Purchase</button>
+            <button onClick={() => handleRemove(id)}>Remove</button>
         </div>
     );
 };
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,14 +1,40 @@
 //use local storage as your db for now
 const addToDb = id => {
-    const exist = localStorage.getItem(id);
-    console.log(exist);
+    const exist = getDb();
+    let shopping_cart = {};
     if (!exist) {
-        localStorage.setItem(id, 1);
+
+        shopping_cart[id] = 1;
+
     }
     else {
-        const newCount = parseInt(exist) + 1;
-        localStorage.setItem(id, newCount);
+        shopping_cart = JSON.parse(exist);
+        if (shopping_cart[id]) {
+            const newCount = shopping_cart[id] + 1;
+            shopping_cart[id] = newCount;
+        }
+        else {
+            shopping_cart[id] = 1;
+        }
     }
 
+    updateDb(shopping_cart);
 }
-export { addToDb }
+
+const getDb = () => localStorage.getItem('shopping_cart');
+const updateDb = cart => {
+    localStorage.setItem('shopping_cart', JSON.stringify(cart));
+}
+
+const removeFromDb = id => {
+    const exist = getDb();
+    if (!exist) {
+
+    }
+    else {
+        const shopping_cart = JSON.parse(exist);
+        delete shopping_cart[id];
+        updateDb(shopping_cart);
+    }
+}
+export { addToDb, removeFromDb }
